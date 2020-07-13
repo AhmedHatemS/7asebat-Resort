c721642ca03eec3b5a6e86499f5f0c41581a995c
AhmedHatemS <dev.ahmedhatem@gmail.com>
2020-07-13 20:55:01 +0200

    add files

--- Ahmed/contact us.js
+++ Ahmed/contact us.js
@@ -1,3 +1,31 @@
-function myFunction() {
-                alert("Message sent, thanks!")
-            }
+function validateForm() 
+{
+    var fname = document.forms["sendMessageForm"]["first-name"].value;
+    var lname = document.forms["sendMessageForm"]["last-name"].value;
+    var mail = document.forms["sendMessageForm"]["email"].value;
+    var message = document.forms["sendMessageForm"]["message"].value;
+    var counter=0;
+    
+    if (fname == "") {
+        alert("First name must be filled out");
+        counter++;
+        return false;
+    }
+    if (lname == "") {
+        alert("Last name must be filled out");
+        counter++;
+        return false;
+    }
+    if (mail == "") {
+        alert("Email must be filled out");
+        counter++;
+        return false;
+    }
+    if (message == "") {
+        alert("Message must be filled out");
+        counter++;
+        return false;
+    }
+    if (counter == 0)
+        alert("Message sent, thanks!")
+}
