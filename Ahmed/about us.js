967644ec975d1337a2ca68a21f5c86f9092f6d47
AhmedHatemS <dev.ahmedhatem@gmail.com>
2020-07-16 14:32:46 +0200

    update files

--- Ahmed/about us.js
+++ Ahmed/about us.js
@@ -1,8 +1,32 @@
+var flag = 1;
+function check(){
+    if(flag == 2){
+        secondClick(); flag = 1;
+    }else{
+        firstClick(); flag = 2;
+    }
+}
+
 $(document).ready(function(){
-          $("#hide").click(function(){
-            $(".text").hide();
-          });  
-          $("#show").click(function(){
+    $("#click-button").click(function(){
+        check(); 
+    });
+});
+
+function firstClick() {
             $(".text").show();
-          });
-        });
+            document.getElementById("Span").innerHTML = "Info enabled.";
+            setTimeout(function(){
+                document.getElementById("Span").innerHTML = "Hide details?";
+                document.getElementById("click-button").style.backgroundColor = "red";
+            }, 1500);
+}
+
+function secondClick() {
+            $(".text").hide();
+            document.getElementById("Span").innerHTML = "Info disabled.";
+            setTimeout(function(){
+                document.getElementById("Span").innerHTML = "Show more?";
+                document.getElementById("click-button").style.backgroundColor = "lime";
+            }, 1500);
+}
